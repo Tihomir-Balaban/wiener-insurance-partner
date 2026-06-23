790afa6aa8d8021d6fa330085707b4d2eaaab4af
Tihomir-Balaban <161501037+Tihomir-Balaban@users.noreply.github.com>
2026-06-23 19:21:26 +0200

    Improve user experience with alerts and validation focus
    Added automatic dismissal for alert-dismissible elements after 5 seconds and enhanced form usability by focusing on the first invalid input field. These changes ensure smoother interactions and better accessibility for users.

--- InsurancePartners.Web/wwwroot/js/site.js
+++ InsurancePartners.Web/wwwroot/js/site.js
@@ -1,1 +1,30 @@
-﻿
+﻿$(function () {
+    const $dismissibleAlerts = $('.alert-dismissible');
+
+    if ($dismissibleAlerts.length > 0) {
+        window.setTimeout(function () {
+            $dismissibleAlerts.alert('close');
+        }, 5000);
+    }
+
+    const $firstInvalidInput = $('.input-validation-error:first');
+
+    if ($firstInvalidInput.length > 0) {
+        $firstInvalidInput.trigger('focus');
+    }
+});
+$(function () {
+    const $dismissibleAlerts = $('.alert-dismissible');
+
+    if ($dismissibleAlerts.length > 0) {
+        window.setTimeout(function () {
+            $dismissibleAlerts.alert('close');
+        }, 5000);
+    }
+
+    const $firstInvalidInput = $('.input-validation-error:first');
+
+    if ($firstInvalidInput.length > 0) {
+        $firstInvalidInput.trigger('focus');
+    }
+});
